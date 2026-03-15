400301ff4734c4cdb9100ded2459ab0ae3ec05b7
Ranjun Singh <ranjun@ranjuns-air.lan>
2026-03-15 14:41:55 -0700

    Remove unused import

--- app/src/main/java/com/example/gymapp/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.gymapp.ui.navigation.GymAppNavGraph
