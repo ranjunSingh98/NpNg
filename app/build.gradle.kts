52a9481cb7daf8e6fd5efca16f38879847cb314b
Ranjun Singh <ranjun@ranjuns-air.lan>
2026-03-21 18:46:30 -0700

    Update application version and build configuration:
    - Increment `versionCode` to 2.
    - Update `versionName` to "0.2".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.gymapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
